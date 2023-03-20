1bc0b3b11797ad2b6460512cfd7373cf1d9eb966
Eli4sW1ls <r0757239@kuleuven.be>
2023-03-20 16:28:26 +0100

    ex12

--- src/routes/02_exercises/01_web_technologies/exercise_12/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_12/Exercise.svelte
@@ -1,10 +1,19 @@
 <script>
 	let clicked = false;
+	const msg_t = "You have clicked the button!";
+	const msg_f = "You have to click the button!";
 </script>
 
 <div>
 	<button class="btn btn-primary" on:click={() => (clicked = true)}>
 		click here
 	</button>
-	<!--  -->
+	
+	<p>
+	{#if clicked}
+		{msg_t}
+	{:else}
+		{msg_f}
+	{/if}
+	</p>
 </div>
