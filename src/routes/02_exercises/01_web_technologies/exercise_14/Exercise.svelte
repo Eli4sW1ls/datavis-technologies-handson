2b3c0a1c9aa0570104bdc79c457c4ecf53dbdc75
Eli4sW1ls <r0757239@kuleuven.be>
2023-03-20 16:41:29 +0100

    ex 14 - web tech done

--- src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
@@ -3,14 +3,21 @@
 </script>
 
 <!-- We will cover buttons later on! -->
-<button class="btn btn-secondary" on:click={() => (current = "messi")}>
+<button class="btn btn-secondary" class:clicked={current=="messi"} on:click={() => (current = "messi")}>
   Messi
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "ronaldo")}>
+<button class="btn btn-secondary" class:clicked={current=="ronaldo"} on:click={() => (current = "ronaldo")}>
   Ronaldo
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "neymar")}>
+<button class="btn btn-secondary" class:clicked={current=="neymar"} on:click={() => (current = "neymar")}>
   Neymar
 </button>
+
+
+<style>
+  .clicked {
+    background-color: coral;
+  }
+</style>
