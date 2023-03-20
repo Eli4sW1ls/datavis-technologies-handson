7c48f8fa72eea95673ff8568280166a778af2fda
Eli4sW1ls <r0757239@kuleuven.be>
2023-03-20 15:40:45 +0100

    ex9

---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,11 @@
+The circumference of a circle with radius {radius} m is {circf} m.
+
+<script>
+    const radius = 4234;
+
+    function compute_circumference(radius) {
+        return (2*Math.PI*radius).toFixed(2)
+    }
+
+    const circf = compute_circumference(radius);
+</script>
