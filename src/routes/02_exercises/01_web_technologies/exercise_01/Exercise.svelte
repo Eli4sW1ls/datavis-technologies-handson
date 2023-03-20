5bbce6ae9d65cc583cc5a63b4f50a10528813c28
Eli4sW1ls <r0757239@kuleuven.be>
2023-03-20 14:31:07 +0100

    ex5 + ex1 corr

--- src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
@@ -1,4 +1,4 @@
-<div class="answer" style="color: rgb(2, 96, 45)">My name is Elias</div>
+<div class="answer" style="color: rgb(2, 96, 45)">My name is <b style="color:red">Elias</b></div>
 
 <!-- <style>
   div.answer {
