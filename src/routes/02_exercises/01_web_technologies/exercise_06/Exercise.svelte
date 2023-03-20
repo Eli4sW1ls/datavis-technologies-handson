e312f2476686587b1210f4992cdc8977e8fe6945
Eli4sW1ls <r0757239@kuleuven.be>
2023-03-20 15:07:18 +0100

    ex6

--- src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
@@ -1,1 +1,13 @@
-I really like data visualisation!
+<div class=prim>I <span class=really>really</span> like data visualisation!</div>
+
+<style>
+    .prim {
+        font-family: "Arial";
+        font-size: 30px;
+        color: steelblue;
+    }
+    
+    .really {
+        color: magenta
+    }
+</style>
