ffc2244283adea5dc333a7950f78742f6eb9d403
Eli4sW1ls <r0757239@kuleuven.be>
2023-03-20 15:29:35 +0100

    ex8

---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,5 @@
+<p>My name is {obj.name} and these are some of my favourite things: {obj['favs']}</p>
+
+<script>
+    const obj = {name: "Elias", favs: ["Music", " Maths", " Friends", " USO", " Data visualization"]}
+</script>
