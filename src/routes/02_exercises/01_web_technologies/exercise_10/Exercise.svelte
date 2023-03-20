61ea478f40a0e231bef3b09fec0f132d86767de4
Eli4sW1ls <r0757239@kuleuven.be>
2023-03-20 15:56:16 +0100

    ex10

---
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -0,0 +1,8 @@
+<div><Ex3/></div>
+<div><Ex2/></div>
+
+
+<script>
+    import Ex2 from '/src/routes/02_exercises/01_web_technologies/exercise_02/Exercise.svelte'
+    import Ex3 from '/src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte'
+</script>
