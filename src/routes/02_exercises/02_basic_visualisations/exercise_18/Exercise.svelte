d17e866670d865819f6a01155f1564dbb55e1050
Eli4sW1ls <r0757239@kuleuven.be>
2023-03-29 15:53:10 +0200

    ex18

--- src/routes/02_exercises/02_basic_visualisations/exercise_18/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_18/Exercise.svelte
@@ -1,18 +1,35 @@
 <script>
-    // Dimensions
-    const width = 800;
-    const height = 100;
-    const margin = { top: 5, right: 5, bottom: 25, left: 5 };
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-  
-    // Array
-    const values = [2, 4, 6, 7, 9];
-  </script>
-  
-  <svg viewBox="0 0 {width} {height}">
-    <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+  // Dimensions
+  const width = 800;
+  const height = 100;
+  const margin = { top: 5, right: 5, bottom: 5, left: 5 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  // Array
+  const values = [2, 4, 6, 7, 9];
+
+  import { scaleLog } from 'd3-scale';
+  const scale = scaleLog()
+      .domain([1,10])
+      .range([0, innerWidth])
+
+  import { axisBottom } from 'd3-axis'
+  import { select } from 'd3-selection'
+  const xAxis = axisBottom(scale)
+
+  function addAxis(handle) {
+    xAxis(select(handle));
+  }
+</script>
+
+<svg viewBox="0 0 {width} {height}">
+  <g transform="translate({margin.left},{margin.top})">
+    {#each values as value}
+      <circle cx={scale(value)} cy={innerHeight/2} r=10 />
+    {/each}
+    <g use:addAxis transform="translate(0, {innerHeight-25})">
+      <text fill="currentColor" font-size=20px x={innerWidth/2} y=30>x</text>
     </g>
-  </svg>
-  
+  </g>
+</svg>
