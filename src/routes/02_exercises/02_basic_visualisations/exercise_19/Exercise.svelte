e1ed71dd0ec8dfb21434d0d17900ad6c5d15e056
Eli4sW1ls <r0757239@kuleuven.be>
2023-03-30 11:12:50 +0200

    ex19

--- src/routes/02_exercises/02_basic_visualisations/exercise_19/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_19/Exercise.svelte
@@ -14,11 +14,27 @@
       { x: 7, y: 3, category: "cat3" },
       { x: 9, y: 1, category: "cat2" }
     ];
+
+    import { scaleLinear, scaleOrdinal } from 'd3-scale';
+    import { schemeSet2 } from 'd3-scale-chromatic'
+    const scaleX = scaleLinear()
+      .domain([0,10])
+      .range([0, innerWidth])
+
+      const scaleY = scaleLinear()
+      .domain([0,3.5])
+      .range([innerHeight, 0])
+
+      const scaleCat = scaleOrdinal(schemeSet2)
+      // const uniques = [...new Set(values.map(v => v.category))]
   </script>
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+      {#each values as value}
+        <circle cx={scaleX(value.x)} cy={scaleY(value.y)} fill={scaleCat(value.category)} r=10 />
+        <text x={scaleX(value.x)} y={scaleY(value.y)-15} fill={scaleCat(value.category)} font-size=15px>y={value.y}</text>
+      {/each}
     </g>
   </svg>
   
