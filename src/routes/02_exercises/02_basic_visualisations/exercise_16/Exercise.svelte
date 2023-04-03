3def4e2fb3e0ed2904a75717ef6ef1c93053c8ad
Eli4sW1ls <r0757239@kuleuven.be>
2023-04-03 12:39:07 +0200

    tweak ex16 + start ex21

--- src/routes/02_exercises/02_basic_visualisations/exercise_16/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_16/Exercise.svelte
@@ -1,6 +1,6 @@
 <script>
-    const width = 100;
-    const height = 100;
+    const width = 400;
+    const height = 400;
     const margin = { top: 5, right: 5, bottom: 5, left: 5 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
--- src/routes/02_exercises/02_basic_visualisations/exercise_21/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_21/Exercise.svelte
@@ -1,3 +1,11 @@
 <script>
+    import { json } from 'd3-fetch';
+    import Scpl from './Scatterplot.svelte'
 
+    let data = null;
+    onMount(async () => {
+        console.log("onMount started!");
+        data = await json('/data/gapminder.json');
+        console.log("Data loaded!");
+    });
 </script>
