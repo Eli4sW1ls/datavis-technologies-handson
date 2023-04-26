3afea1aa719d910bff1a2dd2b48cc41bf6c23677
Eli4sW1ls <r0757239@kuleuven.be>
2023-04-26 18:40:24 +0200

    ex24

--- src/routes/02_exercises/03_interactivity/exercise_23/Exercise.svelte
+++ src/routes/02_exercises/03_interactivity/exercise_23/Exercise.svelte
@@ -1,4 +1,7 @@
 <script>
+import { onMount } from "svelte";
+
+
     // Dimensions
     const width = 800;
     const height = 100;
@@ -17,14 +20,31 @@
   
     // All lights with a higher index are on!
     let index = points.length;
+    let op_list = Array(5).fill(0.3);
   
     // Color
     let color = "darkred";
+
+    function animation(handle) {
+      if (index == -1) {
+        color = 'darkgreen';
+        clearInterval()
+      }
+
+      op_list[index] = 1;
+      index -= 1;
+    }
+
+    onMount(async() => {
+      setInterval(animation, 700);
+    });
   </script>
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+      {#each points as p, i}
+        <circle cx={p} r=10 fill={color} opacity={op_list[i]}/>
+      {/each}
     </g>
   </svg>
   
