0a2530a54200e54d7f75a1ca13f3510ca7409ec4
Eli4sW1ls <r0757239@kuleuven.be>
2023-04-26 17:59:18 +0200

    ex22

--- src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
+++ src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
@@ -1,3 +1,15 @@
 <script>
+    let ctr = 0;
+
+    function clicked(event) {
+        ctr += 1;
+    }
+
+    function reset(event) {
+        ctr = 0;
+    }
     
 </script>
+
+<button on:click={clicked} class="btn btn-primary">Clicked: {ctr}</button> 
+<button on:click={reset} class="btn btn-danger">Reset</button>
